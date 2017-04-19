3c59d494d9aac0074c04217cd251e5b2cc392329
TamLeMinh <minhtambk92@gmail.com>
2017-04-19 13:27:24 +0700

    update functuon render banner HTML

--- build/Arf.js
+++ build/Arf.js
@@ -9755,6 +9755,8 @@ var Entity = function Entity(entity) {
   this.width = entity.width;
   this.height = entity.height;
   this.html = entity.html;
+  this.script = entity.script;
+  this.image = entity.image;
   this.css = entity.outputCss;
   this.cpm = entity.cpm;
 };
@@ -11047,7 +11049,7 @@ var Banner = _vue2.default.component('banner', {
           iframe.scrolling = 'no'; // Prevent iframe body scrolling
 
           iframe.contentWindow.document.open();
-          iframe.contentWindow.document.write(vm.current.html);
+          iframe.contentWindow.document.write(vm.current.script);
           iframe.contentWindow.document.close();
 
           // Prevent scroll on IE
@@ -11063,6 +11065,48 @@ var Banner = _vue2.default.component('banner', {
       } catch (error) {
         throw new Error(error);
       }
+    },
+    renderBannerHTML: function renderBannerHTML() {
+      var _arguments = arguments;
+
+      var vm = this;
+      var iframe = vm.iframe.el;
+      var urlCore = 'http://admicro1.vcmedia.vn/core/admicro_core_nld.js';
+      var admLoadJs = function admLoadJs(c, b) {
+        // eslint-disable-line no-unused-vars, class-methods-use-this
+        var a = document.createElement('script');
+        a.type = 'text/javascript';
+        a.src = c;
+        /* eslint-disable no-unused-expressions */
+        /* eslint-disable no-mixed-operators */
+        _arguments.length >= 2 && (a.onload = b, a.onreadystatechange = function () {
+          a.readyState !== 4 && a.readyState !== 'complete' || b();
+        });
+        document.getElementsByTagName('head')[0].appendChild(a);
+      };
+      admLoadJs(urlCore, function () {
+        var ifrm = iframe;
+        ifrm.style.width = '300px';
+        ifrm.style.height = '600px';
+        ifrm.style.display = 'block';
+        ifrm.style.border = 'none';
+        ifrm.scrolling = 'no';
+        ifrm.allowfullscreen = 'true';
+        ifrm.webkitallowfullscreen = 'true';
+        ifrm.mozallowfullscreen = 'true';
+        ifrm.src = 'about:blank';
+
+        document.getElementById('banner').appendChild(ifrm);
+        /* eslint-disable no-useless-concat */
+
+        window.data = vm.current.html;
+
+        var ifrmlRender = ifrm.contentWindow || ifrm.contentDocument.document || ifrm.contentDocument;
+        ifrmlRender.document.open();
+        ifrmlRender.document.write('<head>' + '<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">' + '<script>inDapIF = true;function mobileCallbackMedium(){window.parent.callbackMedium();}</sc' + 'ript>' + '</head><body style="border: none;display: block;margin: 0 auto;">' + '<scri' + 'pt>' + ' </scr' + 'ipt>' + '<scri' + 'pt src="http://media1.admicro.vn/core/sponsoradx3.js" type="text/javascript"> </scr' + 'ipt>' + '<script>sponsoradx(parent.data)</scr' + 'ipt></body>');
+        ifrmlRender.document.close();
+        document.getElementById('banner').style.display = 'block';
+      });
     }
   },
 
--- src/components/Banner.js
+++ src/components/Banner.js
@@ -72,7 +72,7 @@ const Banner = Vue.component('banner', {
           iframe.scrolling = 'no'; // Prevent iframe body scrolling
 
           iframe.contentWindow.document.open();
-          iframe.contentWindow.document.write(vm.current.html);
+          iframe.contentWindow.document.write(vm.current.script);
           iframe.contentWindow.document.close();
 
           // Prevent scroll on IE
@@ -88,6 +88,53 @@ const Banner = Vue.component('banner', {
       } catch (error) {
         throw new Error(error);
       }
+    },
+    renderBannerHTML() {
+      const vm = this;
+      const iframe = vm.iframe.el;
+      const urlCore = 'http://admicro1.vcmedia.vn/core/admicro_core_nld.js';
+      const admLoadJs = (c, b) => { // eslint-disable-line no-unused-vars, class-methods-use-this
+        const a = document.createElement('script');
+        a.type = 'text/javascript';
+        a.src = c;
+        /* eslint-disable no-unused-expressions */
+        /* eslint-disable no-mixed-operators */
+        arguments.length >= 2 && (a.onload = b, a.onreadystatechange = function () {
+          (a.readyState !== 4) && (a.readyState !== 'complete') || b();
+        });
+        document.getElementsByTagName('head')[0].appendChild(a);
+      };
+      admLoadJs(urlCore, () => {
+        const ifrm = iframe;
+        ifrm.style.width = '300px';
+        ifrm.style.height = '600px';
+        ifrm.style.display = 'block';
+        ifrm.style.border = 'none';
+        ifrm.scrolling = 'no';
+        ifrm.allowfullscreen = 'true';
+        ifrm.webkitallowfullscreen = 'true';
+        ifrm.mozallowfullscreen = 'true';
+        ifrm.src = 'about:blank';
+
+        document.getElementById('banner').appendChild(ifrm);
+        /* eslint-disable no-useless-concat */
+
+        window.data = vm.current.html;
+
+        const ifrmlRender = ifrm.contentWindow || ifrm.contentDocument.document
+          || ifrm.contentDocument;
+        ifrmlRender.document.open();
+        ifrmlRender.document.write(`${'<head>' +
+          '<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">' +
+          '<script>inDapIF = true;function mobileCallbackMedium(){window.parent.callbackMedium();}</sc' + 'ript>' +
+          '</head><body style="border: none;display: block;margin: 0 auto;">' +
+          '<scri' + 'pt>'} </scr` + 'ipt>' +
+          '<scri' + 'pt src="http://media1.admicro.vn/core/sponsoradx3.js" type="text/javascript"> </scr' + 'ipt>' +
+          '<script>sponsoradx(parent.data)</scr' +
+          'ipt></body>');
+        ifrmlRender.document.close();
+        document.getElementById('banner').style.display = 'block';
+      });
     },
   },
 
