bef7ccd8c98e278095456d5d04cf466f6fb4fc0d
Minh Tam Le <minhtambk92@gmail.com>
2017-07-30 17:52:34 +0700

    change cdn host

--- src/template.js
+++ src/template.js
@@ -8,7 +8,7 @@ const env = (location.search.indexOf('corejs_env=dev') !== -1) ? '' : '.min';
 const script = document.createElement('script');
 script.id = 'arf-core-js';
 script.type = 'application/javascript';
-script.src = `http://rawgit.com/minhtambk92/ARF-Sub/master/build/Arf${env}.js`;
+script.src = `http://arf-dev.manhpt.com/Arf${env}.js`;
 
 // Async load core-js script
 if (!document.getElementById(script.id)) {
